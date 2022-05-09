8c31a3437e515a6562e0acae675a275d16b82deb
maggie452 <90792571+maggie452@users.noreply.github.com>
2022-05-09 13:27:53 -0400

    slayage

--- vite-project/catalog.js
+++ vite-project/catalog.js
@@ -57,3 +57,96 @@ const sections = [
 
 export { sections }
 
+const islands = [
+    {
+        name: "Kauai",
+        description: "Kauai is Hawaii's fourth largest island and is sometimes called the 'Garden Island', which is an entirely accurate description. The oldest and northernmost island in the Hawaiian chain is draped in emerald valleys, sharp mountain spires and jagged cliffs aged by time and the elements. Centuries of growth have formed tropical rainforests, forking rivers and cascading waterfalls! Some parts of Kauai are only accessible by sea or air, revealing views beyond your imagination. More than just dramatic beauty, the island is home to a variety of outdoor activities. You can kayak the Wailua River, snorkel on Poipu Beach, hike the trails of Kokee State Park, or go ziplining above Kauai's lush valleys. But, it is the island's laid-back atmosphere and rich culture found in its small towns that make it truly timeless. Explore the regions of Kauai and make your escape to discover the undeniable allure of the island.",
+        img: "/island1.jpg",
+    },
+    {
+        name: "Oahu",
+        description: "Sometimes called 'The Gathering Place', Oahu certainly lives up to its name. The third largest Hawaiian island is home to the majority of Hawaii’s diverse population, a fusion of East and West cultures rooted in the values and traditions of the Native Hawaiian people. It’s this fundamental contrast between the ancient and the modern that makes discovering Oahu — from bustling city life to laidback surf towns — so enjoyable. You'll find plenty of different options for exploring Oahu's different regions, the island's many activities, its eclectic restaurants and accommodations.",
+        img: "/island2.jpg",
+    },
+    {
+        name: "Molokai",
+        description: "Hawaii’s fifth largest island, Molokai is only 38 miles long and 10 miles across at its widest point and is home to the highest sea cliffs in the world and the longest continuous fringing reef. Molokai remains true to its island roots, with a high percentage of its population being of Native Hawaiian ancestry who continue to preserve their rural lifestyle thanks to their love of the land. Whether you’re led by a guide along the cliffs leading to Kalaupapa National Historical Park or discovering Papohaku Beach, one of Hawaii’s largest white-sand beaches, Molokai is truly an island of outdoor adventure where Hawaii’s past comes alive! Check out the regions of Molokai for yourself.",
+        img: "/island3.jpg",
+    },
+    {
+        name: "Lanai",
+        description: "The smallest inhabited island travelers may visit in Hawaii, Lanai offers big enticements to its visitors. Only nine miles from Maui yet a world away, Lanai can feel like two places. The first is found in luxurious resorts where visitors can indulge in world-class amenities and championship-level golf. The other is found bouncing along the island’s rugged back roads in a 4-wheel-drive vehicle to explore off-the-beaten-path treasures. You’re sure to find serenity, adventure and privacy on any of the three regions of Lanai.",
+        img: "/island4.jpg",
+    },
+    {
+        name: "Maui",
+        description: "Maui, known also as 'The Valley Isle', is the second largest Hawaiian island. The island beloved for its world-famous beaches, the sacred Iao Valley, views of migrating humpback whales (during winter months), farm-to-table cuisine and the magnificent sunrise and sunset from Haleakala. It’s not surprising Maui has been voted 'Best Island in the U.S.' by Condé Nast Traveler readers for more than 20 years. Check out the regions of Maui and all this island has to offer.",
+        img: "/island5.jpg",
+    },
+    {
+        name: "Big Island",
+        description: "The island of Hawaii is the youngest and largest island in the Hawaiian chain. Nearly twice as big as all of the other Hawaiian Islands combined (hence, its nickname, “Big Island”), its sheer size is awe-inspiring. You can travel through all but four of the world's different climate zones here, ranging from Wet Tropical to Polar Tundra, a result of the shielding effect and elevations of the massive volcanoes Maunakea and Maunaloa. From the many geological features at Hawaii Volcanoes National Park to the snow-capped heights of Maunakea; from the lush valleys of the Hilo and Hamakua Coasts to the jet-black sands of Punaluu Beach, the island of Hawaii is an unrivaled expression of the power of nature. However you decide to experience the island, it is sure to leave you humbled! Check out the island of Hawaii's regions to explore this vast island.",
+        img: "/island6.jpeg",
+    },
+];
+export { islands }
+
+const volcanoes = [
+    {
+        name: "Hawaii Volcanoes National Park",
+        description: "Hawaii Volcanoes National Park encompasses 335,259 acres or about 523 square miles from the summit of Maunaloa to the sea (by comparison, the island of Oahu is 597 square miles). Discover 150 miles of hiking trails through volcanic craters, scalded deserts and rainforests, as well as a visitor center, petroglyphs and two active volcanoes: Maunaloa, which last erupted in 1984 and and Kilauea which last erupted in 2018 (1983-2018). Kilauea is often called 'the world's only drive-in volcano.' At one time, this prolific volcano produced 250,000-650,00 cubic yards of lava per day - enough to resurface a 20-mile-long, two-lane road each day. As of January 1983, more than 875 acres of new land has been created on the island of Hawaii.The extraordinary natural diversity of the park was recognized in 1980 when it was named a World Biosphere Site by UNESCO and in 1987 when the park was honored as a World Heritage Site.",
+    },
+    {
+        name: "Diamond Head",
+        description: "The unique profile of Diamond Head (Lē‘ahi) sits prominently near the eastern edge of Waikiki’s coastline. Hawaii’s most recognized landmark is known for its historic hiking trail, stunning coastal views, and military history. Diamond Head State Monument encompasses over 475 acres, including the interior and outer slopes of the crater.This broad, saucer-shaped crater was formed about 300,000 years ago during a single, explosive eruption that sent ash and fine particles in the air. As these materials settled, they cemented together into a rock called tuff, creating the crater, which is visible from the trail in the park. Most of the vegetation and birds were introduced in the late 1800s to early 1900s.The trail to the summit of Lē‘ahi was built in 1908 as part of Oahu’s coastal defense system. The 0.8 mile hike from trailhead to the summit is steep and strenuous, gaining 560 feet as it ascends from the crater floor. The walk is a glimpse into the geological and military history of Diamond Head. A concrete walkway built to reduce erosion shifts to a natural tuff surface about 0.2 mile up the trail with many switchbacks traversing the steep slope of the crater interior. The ascent continues up steep stairs and through a lighted 225-foot tunnel to enter the Fire Control Station completed in 1911. Built on the summit, the station directed artillery fire from batteries in Waikiki and Fort Ruger outside Diamond Head crater. At the summit, you’ll see bunkers and a huge navigational lighthouse built in 1917. The postcard view of the shoreline from Koko Head to Wai‘anae is stunning, and during winter, may include passing humpback whales.",
+    },
+];
+export { volcanoes }
+
+const culture = []
+
+const beaches = [
+    {
+        name: "Waikiki Beach",
+        description: "For those who love the beach, look no further than Waikiki. With its long stretch of white sand and turquoise blue water, everyone enjoys this paradise for swimming, surfing, snorkeling, and more. From Hilton Hawaiian Village on the west side to Kapi’olani Park in the east, Waikiki boasts a beautiful tropical climate with tons of activities and events for all ages. Find more high-rises, high-end restaurants, and resorts pepper along this popular destination’s borders. It really should be known as Waikiki Beaches, with the many different sections that make up this two-mile stretch of white sand.  Together, these beach sections make what everyone refers to as Waikiki Beach, one of the busiest and most cosmopolitan spots in Oahu where a plethora of activities geared towards tourists seem to go on all day and most of the night. Whether you are looking forward to surfing, canoe paddling, snorkeling, swimming, boogie boarding, sand combing, catching a golden tan, people watching, or marveling at a few beautiful Waikiki sunsets, it’s very easy to have a good time on the different areas of Waikiki Beach.",
+    },
+    {
+        name: "Makena Beach",
+        description: "Makena Beach State Park is one of Maui's signature beaches. With white sands extending nearly 2/3 of a mile long, it is one of the largest, undeveloped beaches in Maui. Enjoy relaxing on the beach with only nature in sight. Nestled between two black-lava outcroppings, Makena offers protection from the trade winds and provides great views of the islands of Molokini and Kahoolawe. The area is divided into two beaches known as Big Beach and Little Beach. Big Beach is located south of Wailea near the Makena Beach and Golf Resort and provides a secluded alternative to more crowded beaches in Kaanapali and Lahaina. Little Beach is a small cove without amenities and no lifeguard. Although state park regulations prohibit nudity, Little Beach is frequently used as a nude beach.",
+    },
+];
+export { beaches }
+
+const museums = [
+    {
+        name: "Pearl Harbor",
+        description: "There is a reason that Pearl Harbor is the number one visitor destination in Hawaii and the USS Arizona Memorial is where your Pearl Harbor experience begins. Millions of people from all over the world come to this majestic setting to see for themselves where World War II began for the United States on December 7, 1941. The USS Arizona Memorial is a must-see destination for all individuals coming to Hawaii, drawing more than 1.7 million visitors each year from all over the world. Visitors are free to explore the grounds of the $65 million Pearl Harbor Visitor Center, which was expanded from the original 3 to 17 acres, in December of 2011. The USS Arizona Memorial is built over the remains of the sunken battleship USS Arizona, the final resting place for many of the 1,177 crewmen killed on December 7, 1941, when their ship was bombed by Japanese Naval Forces. This loss of life represents over half of the Americans killed during the worst naval disaster in American history.",
+    },
+    {
+        name: "Bishop Museum",
+        description: "Honolulu’s Bishop Museum is Hawaii’s largest museum dedicated to studying and preserving the history of Hawaii and the Pacific. Originally designed to house the extensive collection of Hawaiian artifacts and royal family heirlooms of Princess Bernice Pauahi Bishop, a descendant of King Kamehameha I, the museum is now the premier natural and cultural history institution in the Pacific. One of Oahu’s most historic places, the museum holds millions of artifacts, documents and photos about Hawaii and other Polynesian cultures.",
+    },
+];
+export { museums }
+
+const foods = [
+    {
+        name: "All-Natural Shave Ice",
+        description: "Hawaii’s iconic frozen treat is now part of the Islands’ farm-to-table movement. It’s becoming easier to get a bowl of finely shaved ice over which housemade, all natural, locally sourced syrups are generously poured. In many ways, this evolved shave ice recaptures the cool cones’ history. Japanese laborers, contracted to work in Hawaii sugar and pineapple fields, brought shave ice with them. Plantation workers would use their machetes to finely shave the ice from large ice blocks, then pour fruit juice over the fluffy ice.",
+    },
+    {
+        name: "Poke",
+        description: "Perennially beloved by locals and visitors alike, poke is a rich dish full of flavorful, simple ingredients. Most versions use ahi tuna, chopped into small cubes and combined with onions, sesame seeds, limu seaweed and soy sauce. Sometimes, they substitute other freshwater fish or even octopus instead. The dish is so popular that it has made its way across the ocean, showing up in eateries all along the mainland.",
+    },
+    {
+        name: "Poi",
+        description: "The staple and traditional filler starch dish in Hawaiian cuisine is something known as poi. Poi is a thick paste made from taro root (similar to a yam or potato but with a starchy-er flavor) that is either steamed or baked and pounded. While pounding, water is added to the mixture to create a very sticky pudding-like consistency. Poi has a unique flavor, it’s starchy and slightly sour from the light fermentation in the preparation process.",
+    },
+];
+export { foods }
+
+
+
+
+
+
