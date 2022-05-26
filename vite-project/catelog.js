b645b57a8d8b58532a493e0ad7e327d76e3c7587
sophiac2365 <sophiac632@nycstudents.net>
2022-05-26 12:35:34 -0400

    5/26/2022

--- vite-project/catelog.js
+++ vite-project/catelog.js
@@ -1,61 +1,3 @@
-const sections = [
-  {
-    name: "Food",
-    subname: "All-Natural Shave Ice",
-    description: "Hawaiian shave ice is an ice-based dessert made by shaving a block of ice and flavoring it with syrup and other sweet ingredients. On the Big Island of Hawai'i, it is also referred to as 'ice shave'. In contrast, a snow cone, a similar American dessert, is made with crushed ice rather than shaved ice.",
-    img: "/img1.jpg",
-  },
-  {
-    name: "Museums",
-    subname: "Pearl Harbor",
-    description: "Today, Pearl Harbor remains an active military base, Headquarters of the Pacific Fleet, and a National Historic Landmark that's home to four unique attractions: from the surprise attack on Pearl Harbor that started it all, to the surrender of the Japanese on the deck of the mighty Battleship Missouri, these four historic sites together tell the story of the War in the Pacific, commemorating the accomplishments of a previous generation.",
-    img: "/img2.jpg",
-  },
-  {
-    name: "Beaches",
-    subname: "Waikiki Beach",
-    description: "For those who love the beach, look no further than Waikiki. With its long stretch of white sand and turquoise blue water, everyone enjoys this paradise for swimming, surfing, snorkeling, and more. From Hilton Hawaiian Village on the west side to Kapi’olani Park in the east, Waikiki boasts a beautiful tropical climate with tons of activities and events for all ages. Find more high-rises, high-end restaurants, and resorts pepper along this popular destination’s borders.",
-    img: "/img3.jpg",
-  },
-  {
-    name: "Culture",
-    subname: "Hula",
-    description: "On its surface, hula is the storytelling dance of the Hawaiian Islands. As with all of Hawaiian culture, when you are fortunate enough to learn more about it, much deeper, more powerful and empowering truths may be revealed. Hula can be paired with chants or contemporary music, slow and sentimental in tempo or fast and energetic. But no matter its style, it is all part of a cultural practice of sharing a trove of stories that connect dancers and audiences to the foundation of Hawaiian ancestral knowledge.",
-    img: "/img4.jpg",
-  },
-  {
-    name: "History",
-    subname: "Origins",
-    description: "The Hawaiian Islands were first settled as early as 400 C.E., when Polynesians 2000 miles away traveled to Hawaii’s Big Island in canoes. Highly skilled farmers and fishermen, Hawaiians lived in small communities ruled by chieftains. In 1820, Western traders and whalers came to the islands, bringing with them diseases that devastated the native Hawaiian population. In 1893, American colonists controlled Hawaii’s sugar-based economy, overthrew the kingdom and established the Republic of Hawaii.",
-    img: "/img5.jpeg",
-  },
-  {
-    name: "Nature",
-    subname: "Kalaupapa National Historical Park",
-    description: "It's quiet as you ride on your mule along the 2.9-mile trail to Kalaupapa Peninsula. The sheer cliffs overlook the Pacific, descending from 1,700 feet with Molokai's North Shore Pali just to the east—the tallest sea cliffs in the world as recorded by the Guinness Book of World Records, measuring 3,600 to 3,900 feet. Three miles, 26 switchbacks and 90 minutes of magnificent views later, you're back to sea level in the historic town of Kalaupapa, one of the most remote settlements in Hawaii.",
-    img: "/img6.jpg",
-  },
-  {
-    name: "Volcanoes",
-    subname: "Hawaii Volcanoes National Park",
-    description: "Experience some of nature’s most awe-inspiring wonders at Hawaii Volcanoes National Park. Located 45 miles southwest of Hilo, the park is home to two volcanoes including Kilauea, one of the most active volcanoes on earth. Discover 150 miles of hiking trails through volcanic craters, scalded deserts and rainforests, as well as a visitor center, petroglyphs and two active volcanoes: Maunaloa, which last erupted in 1984 and and Kilauea which last erupted in 2018.",
-    img: "/img7.jpg",
-  },
-  {
-    name: "Legends",
-    subname: "The Legend of Mokoli'l",
-    description: "Mokoli’i refers to 'little lizard', or the remnants of a tail of a legendary lizard whose back is very visible as the Kualoa mountain range. In fact, 'Kualoa' means 'long, large back', referring to the long large back of a giant lizard or dinosaur. Legend has it that Hi’iakaikapoliopele, one of the sisters to Pele, the goddess who lives at Halema’uma’u at the current volcano on Hawai’i island, passed through the Kualoa area one day. This large lizard or mo’o gave chase to her. She killed him, and one sign of this battle is the remnant tail in the ocean, still seen today.",
-    img: "/img8.jpg",
-  },
-  {
-    name: "Islands",
-    subname: "Islands To Visit",
-    description: "Choosing Hawaii is the easy part — but which islands to visit? From tourist magnets to secluded hideaways, follow our guide to find your Hawaiian paradise. There are six major islands to visit in Hawaii: Kauai, Oahu, Molokai, Lanai, Maui, and the island of Hawaii! Each has its own distinct personality, adventures, activities and sights. We invite you to explore the Islands of Aloha to find your own heavenly Hawaii experiences.",
-    img: "/img9.jpeg",
-  },
-];
-export { sections }
-
 const foods = [
   {
     name: "All-Natural Shave Ice",
@@ -139,7 +81,7 @@ const volcanoes = [
   },
   {
     name: "Diamond Head",
-    description: "The unique profile of Diamond Head (Lē‘ahi) sits prominently near the eastern edge of Waikiki’s coastline. Hawaii’s most recognized landmark is known for its historic hiking trail, stunning coastal views, and military history. Diamond Head State Monument encompasses over 475 acres, including the interior and outer slopes of the crater.This broad, saucer-shaped crater was formed about 300,000 years ago during a single, explosive eruption that sent ash and fine particles in the air. As these materials settled, they cemented together into a rock called tuff, creating the crater, which is visible from the trail in the park. Most of the vegetation and birds were introduced in the late 1800s to early 1900s.The trail to the summit of Lē‘ahi was built in 1908 as part of Oahu’s coastal defense system. The 0.8 mile hike from trailhead to the summit is steep and strenuous, gaining 560 feet as it ascends from the crater floor. The walk is a glimpse into the geological and military history of Diamond Head. A concrete walkway built to reduce erosion shifts to a natural tuff surface about 0.2 mile up the trail with many switchbacks traversing the steep slope of the crater interior. The ascent continues up steep stairs and through a lighted 225-foot tunnel to enter the Fire Control Station completed in 1911. Built on the summit, the station directed artillery fire from batteries in Waikiki and Fort Ruger outside Diamond Head crater. At the summit, you’ll see bunkers and a huge navigational lighthouse built in 1917. The postcard view of the shoreline from Koko Head to Wai‘anae is stunning, and during winter, may include passing humpback whales.",
+    description: "The unique profile of Diamond Head (Lē‘ahi) sits prominently near the eastern edge of Waikiki’s coastline. Hawaii’s most recognized landmark is known for its historic hiking trail, stunning coastal views, and military history. Diamond Head State Monument encompasses over 475 acres, including the interior and outer slopes of the crater.This broad, saucer-shaped crater was formed about 300,000 years ago during a single, explosive eruption that sent ash and fine particles in the air. As these materials settled, they cemented together into a rock called tuff, creating the crater, which is visible from the trail in the park. Most of the vegetation and birds were introduced in the late 1800s to early 1900s.The trail to the summit of Lē‘ahi was built in 1908 as part of Oahu’s coastal defense system. The 0.8 mile hike from trailhead to the summit is steep and strenuous, gaining 560 feet as it ascends from the crater floor. The walk is a glimpse into the geological and military history of Diamond Head.",
     img: "/volcano2.jpg"
   },
 ];
@@ -157,7 +99,7 @@ export { legends }
 const islands = [
   {
     name: "Kauai",
-    description: "Kauai is Hawaii's fourth largest island and is sometimes called the 'Garden Island', which is an entirely accurate description. The oldest and northernmost island in the Hawaiian chain is draped in emerald valleys, sharp mountain spires and jagged cliffs aged by time and the elements. Centuries of growth have formed tropical rainforests, forking rivers and cascading waterfalls! Some parts of Kauai are only accessible by sea or air, revealing views beyond your imagination. More than just dramatic beauty, the island is home to a variety of outdoor activities. You can kayak the Wailua River, snorkel on Poipu Beach, hike the trails of Kokee State Park, or go ziplining above Kauai's lush valleys. But, it is the island's laid-back atmosphere and rich culture found in its small towns that make it truly timeless. Explore the regions of Kauai and make your escape to discover the undeniable allure of the island.",
+    description: "Kauai is Hawaii's fourth largest island and is sometimes called the 'Garden Island', which is an entirely accurate description. The oldest and northernmost island in the Hawaiian chain is draped in emerald valleys, sharp mountain spires and jagged cliffs aged by time and the elements. Centuries of growth have formed tropical rainforests, forking rivers and cascading waterfalls! Some parts of Kauai are only accessible by sea or air, revealing views beyond your imagination. More than just dramatic beauty, the island is home to a variety of outdoor activities. You can kayak the Wailua River, snorkel on Poipu Beach, hike the trails of Kokee State Park, or go ziplining above Kauai's lush valleys.",
     img: "/island1.jpg",
   },
   {
@@ -182,7 +124,7 @@ const islands = [
   },
   {
     name: "Big Island",
-    description: "The island of Hawaii is the youngest and largest island in the Hawaiian chain. Nearly twice as big as all of the other Hawaiian Islands combined (hence, its nickname, “Big Island”), its sheer size is awe-inspiring. You can travel through all but four of the world's different climate zones here, ranging from Wet Tropical to Polar Tundra, a result of the shielding effect and elevations of the massive volcanoes Maunakea and Maunaloa. From the many geological features at Hawaii Volcanoes National Park to the snow-capped heights of Maunakea; from the lush valleys of the Hilo and Hamakua Coasts to the jet-black sands of Punaluu Beach, the island of Hawaii is an unrivaled expression of the power of nature. However you decide to experience the island, it is sure to leave you humbled! Check out the island of Hawaii's regions to explore this vast island.",
+    description: "The island of Hawaii is the youngest and largest island in the Hawaiian chain. Nearly twice as big as all of the other Hawaiian Islands combined (hence, its nickname, “Big Island”), its sheer size is awe-inspiring. You can travel through all but four of the world's different climate zones here, ranging from Wet Tropical to Polar Tundra, a result of the shielding effect and elevations of the massive volcanoes Maunakea and Maunaloa. From the many geological features at Hawaii Volcanoes National Park to the snow-capped heights of Maunakea; from the lush valleys of the Hilo and Hamakua Coasts to the jet-black sands of Punaluu Beach, the island of Hawaii is an unrivaled expression of the power of nature.",
     img: "/island6.jpeg",
   },
 ];
--- vite-project/main.js
+++ vite-project/main.js
@@ -1,12 +1,15 @@
 import './style.css'
-import { sections, foods, museums, beaches, cultures, histories, nature, volcanoes, legends, islands } from './catelog';
+import { foods, museums, beaches, cultures, histories, nature, volcanoes, legends, islands } from './catelog';
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Flip } from "gsap/Flip";
+gsap.registerPlugin(ScrollTrigger, Flip);
 
 const tl = gsap.timeline({ scrollTrigger: ".header-title", delay: 0.5 }); //sequence animations easily
 
 tl.from("#header", { opacity: 0, duration: 1, ease: "ease-out" });
 tl.from(".btn", { x: -1000, duration: 0.5, ease: "ease-out" });
-tl.from(".island-card", { duration: 1, flip: "flip" });
+tl.from(".parent", { duration: 1, flip: "flip" });
 
 const DOMSelectors = {
   card: document.querySelector(".card"),
@@ -37,7 +40,7 @@ DOMSelectors.themebtn.addEventListener("click", function () {
 foods.forEach((food, index) => {
   DOMSelectors.fooddisplay.insertAdjacentHTML(
     "beforeend",
-    `<div class="food card id="${index}">
+    `<div class="food card flex-card" id="${index}">
       <div class="name">${food.name}</div>
       <img class="img" src="${food.img}" alt="${food.name}">
       <p class="description">${food.description}</p>
@@ -48,18 +51,18 @@ foods.forEach((food, index) => {
 museums.forEach((museum, index) => {
   DOMSelectors.museumdisplay.insertAdjacentHTML(
     "beforeend",
-    `<div class="museums card id="${index}">
+    `<div class="museums card" id="${index}">
       <div class="name">${museum.name}</div> 
-      <img class="img" src="${museum.img}" alt="${museum.name}">
-      <p class="description">${museum.description}</p>
-      </div>`
+        <img class="img" src="${museum.img}" alt="${museum.name}">
+        <p class="description">${museum.description}</p>
+    </div>`
   );
 });
 
 beaches.forEach((beach, index) => {
   DOMSelectors.beachdisplay.insertAdjacentHTML(
     "beforeend",
-    `<div class="beaches card id="${index}">
+    `<div class="beaches card" id="${index}">
       <div class="name">${beach.name}</div> 
       <img class="img" src="${beach.img}" alt="${beach.name}">
       <p class="description">${beach.description}</p>
@@ -70,7 +73,7 @@ beaches.forEach((beach, index) => {
 cultures.forEach((culture, index) => {
   DOMSelectors.culturedisplay.insertAdjacentHTML(
     "beforeend",
-    `<div class="cultures card id="${index}">
+    `<div class="cultures card" id="${index}">
       <div class="name">${culture.name}</div> 
       <img class="img" src="${culture.img}" alt="${culture.name}">
       <p class="description">${culture.description}</p>
@@ -81,7 +84,7 @@ cultures.forEach((culture, index) => {
 histories.forEach((history, index) => {
   DOMSelectors.historydisplay.insertAdjacentHTML(
     "beforeend",
-    `<div class="history card id="${index}">
+    `<div class="history card" id="${index}">
       <div class="name">${history.name}</div> 
       <img class="img" src="${history.img}" alt="${history.name}">
       <p class="description">${history.description}</p>
@@ -92,7 +95,7 @@ histories.forEach((history, index) => {
 nature.forEach((nature, index) => {
   DOMSelectors.naturedisplay.insertAdjacentHTML(
     "beforeend",
-    `<div class="nature card id="${index}">
+    `<div class="nature card" id="${index}">
       <div class="name">${nature.name}</div> 
       <img class="img" src="${nature.img}" alt="${nature.name}">
       <p class="description">${nature.description}</p>
@@ -105,8 +108,8 @@ volcanoes.forEach((volcano, index) => {
     "beforeend",
     `<div class="volcanoes card" id="${index}">
       <div class="name">${volcano.name}</div>
-      <img class="img" src="${volcano.img}" alt="${volcano.name}">
-      <p class="description">${volcano.description}</p>
+        <img class="img" src="${volcano.img}" alt="${volcano.name}">
+        <p class="description">${volcano.description}</p>
     </div>`
   );
 });
@@ -125,7 +128,7 @@ legends.forEach((legend, index) => {
 islands.forEach((island, index) => {
   DOMSelectors.islanddisplay.insertAdjacentHTML(
     "beforeend",
-    `<div class="card island-card" id="${index}">
+    `<div class="islands card island-card flex-card" id="${index}">
       <div class="name">${island.name}</div>
       <img class="island-img" src="${island.img}" alt="${island.name}">
       <p class="description">${island.description}</p>
